e6d1fb0f5cfd0cd1ba7c862aec5e56baf2a92a05
cootcode <dawid.grabowski19@gmail.com>
2023-10-19 16:53:06 +0100

    Add project files.

---
+++ Flappy bird/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Flappy_bird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.Ground = new System.Windows.Forms.PictureBox();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.FlappyMosquito = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FlappyMosquito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(12, 9);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(123, 29);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score: 0";
+            // 
+            // Ground
+            // 
+            this.Ground.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Ground.Image = global::Flappy_bird.Properties.Resources.ground;
+            this.Ground.Location = new System.Drawing.Point(-30, 554);
+            this.Ground.Name = "Ground";
+            this.Ground.Size = new System.Drawing.Size(615, 118);
+            this.Ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ground.TabIndex = 3;
+            this.Ground.TabStop = false;
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Image = global::Flappy_bird.Properties.Resources.pipe;
+            this.pipeBottom.Location = new System.Drawing.Point(205, 402);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(100, 216);
+            this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom.TabIndex = 2;
+            this.pipeBottom.TabStop = false;
+            // 
+            // FlappyMosquito
+            // 
+            this.FlappyMosquito.Image = global::Flappy_bird.Properties.Resources.mosquito_insect_side_view;
+            this.FlappyMosquito.Location = new System.Drawing.Point(25, 228);
+            this.FlappyMosquito.Name = "FlappyMosquito";
+            this.FlappyMosquito.Size = new System.Drawing.Size(19, 17);
+            this.FlappyMosquito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FlappyMosquito.TabIndex = 1;
+            this.FlappyMosquito.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pipeTop.Image = global::Flappy_bird.Properties.Resources.pipedown;
+            this.pipeTop.Location = new System.Drawing.Point(336, -9);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(96, 210);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 0;
+            this.pipeTop.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(514, 614);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.Ground);
+            this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.FlappyMosquito);
+            this.Controls.Add(this.pipeTop);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird - Dawid Grabowski";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FlappyMosquito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox FlappyMosquito;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.PictureBox Ground;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Flappy bird/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_bird
+{
+    public partial class Form1 : Form
+    {
+
+        int pipeSpeed = 8;
+        int gravity = 3;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void endGame()
+        {
+            // this is the game end function, this function will when the bird touches the ground or the pipes
+            gameTimer.Stop(); // stop the main timer
+            scoreText.Text += " Game over!!!"; // show the game over text on the score text, += is used to add the new string of text next to the score instead of overriding it
+        }
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            FlappyMosquito.Top += gravity;
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+
+            scoreText.Text = "Score: " + score;
+            if (pipeBottom.Left < -150)
+            {
+                // if the bottom pipes location is -150 then we will reset it back to 800 and add 1 to the score
+                pipeBottom.Left = 800;
+                score++;
+            }
+            if (pipeTop.Left < -180)
+            {
+                // if the top pipe location is -180 then we will reset the pipe back to the 950 and add 1 to the score
+                pipeTop.Left = 950;
+                score++;
+            }
+            if (FlappyMosquito.Bounds.IntersectsWith(pipeBottom.Bounds) ||
+                FlappyMosquito.Bounds.IntersectsWith(pipeTop.Bounds) ||
+                FlappyMosquito.Bounds.IntersectsWith(Ground.Bounds) || FlappyMosquito.Top < -25
+                ) 
+            {
+                // if any of the conditions are met from above then we will run the end game function
+                endGame();
+            }
+
+
+            // if score is greater then 5 then we will increase the pipe speed to 15
+            if (score > 5)
+            {
+                pipeSpeed = 15;
+            }
+        }
+
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            // this is the game key is up event thats linked to the main form
+
+            if (e.KeyCode == Keys.Space)
+            {
+                // if the space key is released then gravity is set back to 15
+                gravity = -15;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            // this is the game key is up event thats linked to the main form
+
+            if (e.KeyCode == Keys.Space)
+            {
+                // if the space key is released then gravity is set back to 15
+                gravity = 15;
+            }
+        }
+    }
+}
